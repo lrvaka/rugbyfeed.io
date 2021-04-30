cdb1a48f74d601e0af2d5f11fc6e04b0f9933fd6
lukevaka <lukevakasisikakala@gmail.com>
2021-04-30 19:30:38 -0400

    display in a grid form

--- src/components/SeasonStandings/StandingsGrid/StandingsGrid.js
+++ src/components/SeasonStandings/StandingsGrid/StandingsGrid.js
@@ -1,11 +1,25 @@
+import React from "react";
 import styles from "./StandingsGrid.module.css";
 import StandingsGridItems from "./StandingsGridItems/StandingsGridItems";
 
-export default function StandingsGrid({data}) {
-console.log(data);
+export default function StandingsGrid({ data }) {
+  console.log(data);
   return (
     <div className={styles.StandingsGrid}>
-    <StandingsGridItems data={data}/>
+      <h4>Rank</h4>
+      <h4>Team</h4>
+      <h4>Record</h4>
+      <h4>Points</h4>
+      <h4>PointDiff</h4>
+      {data.map((e) => (
+        <React.Fragment>
+          <h1>{e.rank}</h1>
+          <h1>{e.team}</h1>
+          <h1>{e.record}</h1>
+          <h1>{e.points}</h1>
+          <h1>{e.pointsDiff}</h1>
+        </React.Fragment>
+      ))}
     </div>
   );
 }
--- src/components/SeasonStandings/StandingsGrid/StandingsGridItems/StandingsGridItems.js
+++ src/components/SeasonStandings/StandingsGrid/StandingsGridItems/StandingsGridItems.js
@@ -3,15 +3,9 @@ import StandingsGridItem from "./StandingsGridItem/StandingsGridItem";
 export default function StandingsGridItems({ data }) {
   return (
     <div>
-      {data.map((e) => (
-        <StandingsGridItem
-          rank={e.rank}
-          team={e.team}
-          record={e.record}
-          points={e.points}
-          pointsDiff={e.pointsDiff}
-        />
-      ))}
+        {data.map(e => (
+            <div>{e.rank}</div>
+        ))}
     </div>
   );
 }
