0fcf1c2a93af82d2ee759a2fc3672527fe9c85f6
Luke Vakasisikakala <lukevakasisikakala@Lukes-Air.home>
2021-06-03 23:48:53 -0400

    fetch-standings fix

--- functions/fetch-standings/fetch-standings.js
+++ functions/fetch-standings/fetch-standings.js
@@ -1,34 +1,16 @@
 const fetch = require("node-fetch");
 require("dotenv").config();
 
-const handler = async function (event, context) {
+const handler = async (event, context) => {
   const api_key = process.env.API_KEY;
   const leagueID = event.queryStringParameters.leagueID;
-
-  try {
-    const response = await fetch(
-      `https://api.b365api.com/v2/league/table?token=${api_key}&league_id=${leagueID}`,
-      {
-        headers: { Accept: "application/json" },
-      }
-    );
-    if (!response.ok) {
-      // NOT res.status >= 200 && res.status < 300
-      return { statusCode: response.status, body: response.statusText };
-    }
-    
-    const data = await response.json();
-    console.log(data);
-    return data;
-  } catch (error) {
-    // output to netlify function log
-    console.log(error);
-    return {
-      statusCode: 500,
-      // Could be a custom message or object i.e. JSON.stringify(err)
-      body: JSON.stringify({ msg: error.message }),
-    };
-  }
+  const response = await fetch(
+    `https://api.b365api.com/v2/league/table?token=${api_key}&league_id=${leagueID}`
+  );
+  console.log(response);
+  const data = await response.json();
+  console.log(data);
+  return data;
 };
 
 module.exports = { handler };
