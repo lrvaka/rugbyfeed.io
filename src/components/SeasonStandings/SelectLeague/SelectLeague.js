8cc7438917511daa75ccfe3cb3f792754213a50b
Luke Vakasisikakala <lukevakasisikakala@Lukes-Air.home>
2021-06-04 16:59:27 -0400

    adsense component

--- src/components/SeasonStandings/SelectLeague/SelectLeague.js
+++ src/components/SeasonStandings/SelectLeague/SelectLeague.js
@@ -1,18 +1,35 @@
+import React from "react";
+
 export default function SelectLeague({ onSelectLeague }) {
   const onChangeHandler = (e) => {
     onSelectLeague(e.target.value);
   };
 
   return (
-    <select onChange={onChangeHandler}>
-      <option value="0">Select competition:</option>
-      {/* <option value="23210">Super Rugby NZ</option>
+    <React.Fragment>
+      <select onChange={onChangeHandler}>
+        <option value="0">Select competition:</option>
+        {/* <option value="23210">Super Rugby NZ</option>
       <option value="23341">Super Rugby AU</option> */}
-      <option value="9721">Major League Rugby</option>
-      <option value="11970">Premiership Rugby</option>
-      <option value="11969">Pro 14</option>
-      <option value="596">Top 14</option>
-      <option value="25813">Super Rugby Trans-Tasman</option>
-    </select>
+        <option value="9721">Major League Rugby</option>
+        <option value="11970">Premiership Rugby</option>
+        <option value="11969">Pro 14</option>
+        <option value="596">Top 14</option>
+        <option value="25813">Super Rugby Trans-Tasman</option>
+      </select>
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+      ></script>
+      <ins
+        class="adsbygoogle"
+        style={{ display: `block` }}
+        data-ad-format="fluid"
+        data-ad-layout-key="-fb+5w+4e-db+86"
+        data-ad-client="ca-pub-3854548660961412"
+        data-ad-slot="5988072436"
+      ></ins>
+      <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
+    </React.Fragment>
   );
 }
