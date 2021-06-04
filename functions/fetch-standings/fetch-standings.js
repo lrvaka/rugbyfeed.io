c9660de78ea8116ea32ae16a615f22ece81291eb
Luke Vakasisikakala <lukevakasisikakala@Lukes-Air.home>
2021-06-03 22:58:04 -0400

    console.log the ting

--- functions/fetch-standings/fetch-standings.js
+++ functions/fetch-standings/fetch-standings.js
@@ -1,27 +1,31 @@
-const fetch = require('node-fetch')
+const fetch = require("node-fetch");
 
 const handler = async function (e) {
   const api_key = process.env.API_KEY;
   try {
-    const response = await fetch(`https://api.b365api.com/v2/league/table?token=${api_key}&league_id=${e}`, {
-      headers: { Accept: 'application/json' },
-    })
+    const response = await fetch(
+      `https://api.b365api.com/v2/league/table?token=${api_key}&league_id=${e}`,
+      {
+        headers: { Accept: "application/json" },
+      }
+    );
+    console.log(response);
     if (!response.ok) {
       // NOT res.status >= 200 && res.status < 300
-      return { statusCode: response.status, body: response.statusText }
+      return { statusCode: response.status, body: response.statusText };
     }
-    const data = await response.json()
+    const data = await response.json();
 
     return data;
   } catch (error) {
     // output to netlify function log
-    console.log(error)
+    console.log(error);
     return {
       statusCode: 500,
       // Could be a custom message or object i.e. JSON.stringify(err)
       body: JSON.stringify({ msg: error.message }),
-    }
+    };
   }
-}
+};
 
-module.exports = { handler }
+module.exports = { handler };
