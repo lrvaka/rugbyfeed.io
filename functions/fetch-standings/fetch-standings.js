93171e3799df4c4ef18d2894e2e9d9098f97e329
Luke Vakasisikakala <lukevakasisikakala@Lukes-Air.home>
2021-06-03 23:37:49 -0400

    pass league id correctly

--- functions/fetch-standings/fetch-standings.js
+++ functions/fetch-standings/fetch-standings.js
@@ -1,16 +1,33 @@
 const fetch = require("node-fetch");
 require("dotenv").config();
 
-const handler = async (request, response) => {
-  const leagueID = request;
+const handler = async function (event, context) {
   const api_key = process.env.API_KEY;
-  const standings = await fetch(
-    `https://api.b365api.com/v2/league/table?token=${api_key}&league_id=${leagueID}`
-  );
-  console.log(standings);
-  const data = await standings.json();
-  console.log(data);
-  response.json(data);
+  const leagueID = event.queryStringParameters.leagueID;
+
+  try {
+    const response = await fetch(
+      `https://api.b365api.com/v2/league/table?token=${api_key}&league_id=${leagueID}`,
+      {
+        headers: { Accept: "application/json" },
+      }
+    );
+    if (!response.ok) {
+      // NOT res.status >= 200 && res.status < 300
+      return { statusCode: response.status, body: response.statusText };
+    }
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    // output to netlify function log
+    console.log(error);
+    return {
+      statusCode: 500,
+      // Could be a custom message or object i.e. JSON.stringify(err)
+      body: JSON.stringify({ msg: error.message }),
+    };
+  }
 };
 
 module.exports = { handler };
