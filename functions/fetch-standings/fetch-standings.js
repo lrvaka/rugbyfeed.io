46d468748aaddde04312a479b174ba0fd7243c16
Luke Vakasisikakala <lukevakasisikakala@Lukes-Air.home>
2021-06-04 00:12:49 -0400

    fetch-standings fix

--- functions/fetch-standings/fetch-standings.js
+++ functions/fetch-standings/fetch-standings.js
@@ -1,18 +1,34 @@
 const fetch = require("node-fetch");
 
-const API_ENDPOINT = "https://icanhazdadjoke.com/";
-
-exports.handler = async (event, context) => {
+const handler = async function (event, context) {
   const api_key = process.env.API_KEY;
   const leagueID = event.queryStringParameters.leagueID;
-  return fetch(
-    `https://api.b365api.com/v2/league/table?token=${api_key}&league_id=${leagueID}`,
-    { headers: { Accept: "application/json" } }
-  )
-    .then((response) => response.json())
-    .then((data) => data)
-    .catch((error) => ({ statusCode: 422, body: String(error) }));
+  try {
+    const response = await fetch(`https://api.b365api.com/v2/league/table?token=${api_key}&league_id=${leagueID}`, {
+      headers: { Accept: "application/json" },
+    });
+    if (!response.ok) {
+      // NOT res.status >= 200 && res.status < 300
+      return { statusCode: response.status, body: response.statusText };
+    }
+    const data = await response.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ msg: data.joke }),
+    };
+  } catch (error) {
+    // output to netlify function log
+    console.log(error);
+    return {
+      statusCode: 500,
+      // Could be a custom message or object i.e. JSON.stringify(err)
+      body: JSON.stringify({ msg: error.message }),
+    };
+  }
 };
+
+module.exports = { handler };
 
 // const fetch = require("node-fetch");
 // require("dotenv").config();
