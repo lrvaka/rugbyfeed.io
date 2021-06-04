ce42ca6c27ccd751971b8b07f03342a8a0c39e32
Luke Vakasisikakala <lukevakasisikakala@Lukes-Air.home>
2021-06-03 23:16:37 -0400

    request

--- functions/fetch-standings/fetch-standings.js
+++ functions/fetch-standings/fetch-standings.js
@@ -1,7 +1,8 @@
 const fetch = require("node-fetch");
+require("dotenv").config();
 
 const handler = async (request, response) => {
-  const leagueID = request.params.leagueID;
+  const leagueID = request;
   const api_key = process.env.API_KEY;
   const standings = await fetch(
     `https://api.b365api.com/v2/league/table?token=${api_key}&league_id=${leagueID}`
